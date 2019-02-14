267b8f5513f063358090da570c4c27c632aaa599
史远 <shiyuan@films.cn>
2019-02-14 11:43:19 +0800

    ''

--- 效果/JS/js效果/js/opacity_rotation.js
+++ 效果/JS/js效果/js/opacity_rotation.js
@@ -1,24 +1,24 @@
-window.onload=function(){
-    var box=getClass("box");
-    var img=document.getElementsByTagName("img");
-    var num=0;
-    setInterval(move,2000);
-    function move(){
+window.onload = function () {
+    let img = document.getElementsByTagName("img");
+    let num = 0;
+    setInterval(move, 2000);
+    function move() {
         num++;
-        if(num>=img.length){
-            num=0;
+        if (num >= img.length) {
+            num = 0;
         }
-        for(var i=0;i<img.length;i++){
-            for(var j=0;j<img.length;j++){
-                img[j].style.opacity=0;
-                if(num-1==-1){
-                    animate(img[0],{opacity:0},800);
-                }else{
-                    animate(img[num-1],{opacity:0},800);
+        //opacity 0 -> 0.3 -> 1
+        for (let i = 0; i < img.length; i++) {
+            for (let j = 0; j < img.length; j++) {
+                img[j].style.opacity = 0;
+                if (num - 1 == -1) {
+                    animate(img[0], {opacity: 0}, 800);
+                } else {
+                    animate(img[num - 1], {opacity: 0}, 800);
                 }
-                img[num].style.opacity=0.3;
+                img[num].style.opacity =0.3;
             }
-            animate(img[num],{opacity:1},1000);
+            animate(img[num], {opacity: 1}, 1000);
         }
     }
-};
+}
