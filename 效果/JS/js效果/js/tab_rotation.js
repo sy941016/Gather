a12a40bd7ea5f62c3fd1a4fc58ee41bc9f52c8a9
史远 <shiyuan@films.cn>
2019-02-21 18:33:13 +0800

    '重构选项卡轮播的js，并添加注释'

--- 效果/JS/js效果/js/tab_rotation.js
+++ 效果/JS/js效果/js/tab_rotation.js
@@ -1,27 +1,24 @@
 window.onload = function () {
     let bigbox = document.getElementsByClassName("bigbox")[0];
     let box = document.getElementsByClassName("box");
-    let t = setInterval(move, 2000);
-    let num = 0;
     let btn = document.getElementsByClassName("btn");
     let rightbtn = document.getElementsByClassName("rightbtn")[0];
     let leftbtn = document.getElementsByClassName("leftbtn")[0];
+    let t = setInterval(move, 2000);
+    let num = 0;
+
 
     function move() {
-        num++;
-        if (num >= box.length) {
-            num = 0;
-        }
+        num >= box.length - 1 ? num = 0 : num++;
         for (let i = 0; i < box.length; i++) {
             for (let j = 0; j < box.length; j++) {
                 box[j].style.zIndex = 0;
                 btn[j].style.backgroundColor = "#000";
             }
-            box[num].style.zIndex = 1;
-            btn[num].style.backgroundColor = "orange";
+          Btn(num)
         }
     }
-
+    //btn交互的处理函数
     for (let i = 0; i < btn.length; i++) {
         btn[i].index = i;
         btn[i].onmouseover = function () {
@@ -30,14 +27,11 @@ window.onload = function () {
                 box[j].style.zIndex = 0;
                 btn[j].style.backgroundColor = "#000";
             }
-            btn[this.index].style.backgroundColor = "orange";
-            box[this.index].style.zIndex = 1;
+           Btn(this.index);
+           num = this.index;
         };
-        btn[i].onmouseout = function () {
-            t = setInterval(move, 3000);
-            num = this.index;
-        }
     }
+    //左右点击的处理函数
     rightbtn.onclick = function () {
         move();
     };
@@ -48,6 +42,21 @@ window.onload = function () {
         }
         move();
     };
+   //鼠标移入移出的处理函数
+    leftbtn.onmouseover = rightbtn.onmouseover = function () {
+        clearTimeout(t);
+    };
+    leftbtn.onmouseout = rightbtn.onmouseout = function () {
+        t = setInterval(move, 2000)
+    };
+
+    //处理btn颜色
+    function Btn(num){
+        box[num].style.zIndex = 1;
+        btn[num].style.backgroundColor = "orange";
+    }
+
+    //左右按钮效果，优化用户体验
     bigbox.onmouseover = function () {
         animate(leftbtn, {opacity: 1}, 200);
         animate(rightbtn, {opacity: 1}, 200);
@@ -55,11 +64,5 @@ window.onload = function () {
     bigbox.onmouseout = function () {
         animate(leftbtn, {opacity: 0}, 200);
         animate(rightbtn, {opacity: 0}, 200);
-    };
-    leftbtn.onmouseover = rightbtn.onmouseover = function () {
-        clearTimeout(t);
-    };
-    leftbtn.onmouseout = rightbtn.onmouseout = function () {
-        t = setInterval(move, 2000)
     };
 };
